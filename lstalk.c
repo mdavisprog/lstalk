eed089c523512a9e77b4e9ca575aafa2fe947e32
mdavisprog <mdavisprog@gmail.com>
2023-01-03 22:57:01 -0800

    lstalk: Add basic dynamic array structure.

--- lstalk.c
+++ lstalk.c
@@ -1,6 +1,72 @@
 #include "lstalk.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+
+//
+// Dynamic Array
+//
+// Very simple dynamic array structure to aid in managing storage and
+// retrieval of data.
+
+typedef struct Vector {
+    char* data;
+    size_t element_size;
+    size_t length;
+    size_t capacity;
+} Vector;
+
+Vector vector_create(size_t element_size) {
+    Vector result;
+    result.element_size = element_size;
+    result.length = 0;
+    result.capacity = 1;
+    result.data = malloc(element_size * result.capacity);
+    return result;
+}
+
+void vector_destroy(Vector* vector) {
+    if (vector == NULL) {
+        return;
+    }
+
+    if (vector->data != NULL) {
+        free(vector->data);
+    }
+
+    vector->data = NULL;
+    vector->element_size = 0;
+    vector->length = 0;
+    vector->capacity = 0;
+}
+
+void vector_push(Vector* vector, void* element) {
+    if (vector == NULL || element == NULL || vector->element_size == 0) {
+        return;
+    }
+
+    if (vector->length == vector->capacity) {
+        vector->capacity = vector->capacity * 2;
+        vector->data = realloc(vector->data, vector->element_size * vector->capacity);
+    }
+
+    size_t offset = vector->length * vector->element_size;
+    memcpy(vector->data + offset, element, vector->element_size);
+    vector->length++;
+}
+
+char* vector_get(Vector* vector, size_t index) {
+    if (vector == NULL || vector->element_size == 0 || index >= vector->length) {
+        return NULL;
+    }
+
+    return &vector->data[vector->element_size * index];
+}
+
+//
+// lstalk API
+//
+// This is the beginning of the exposed API functions for the library.
 
 int lstalk_init() {
     printf("Initialized lstalk version 0.0.1!\n");
