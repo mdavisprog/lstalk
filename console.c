d3cca288fa21542ce4b038ef95eba7d71e41a1da
mdavisprog <mdavisprog@gmail.com>
2023-01-14 12:59:54 -0800

    Add non-blocking user input support on Linux.

--- console.c
+++ console.c
@@ -1,5 +1,6 @@
 #include "lstalk.h"
 #include <stdio.h>
+#include <string.h>
 
 #define INPUT_BUFFER_SIZE 2048
 
@@ -8,7 +9,18 @@
     #define WIN32_LEAN_AND_MEAN
     #include <Windows.h>
     #include <wchar.h>
+#elif __linux__
+    #define LINUX 1
+    #include <sys/select.h>
+    #include <sys/time.h>
+    #include <unistd.h>
+#endif
 
+#if LINUX
+#define POSIX 1
+#endif
+
+#if WINDOWS
 wchar_t user_input_buffer[INPUT_BUFFER_SIZE / 2];
 size_t user_input_index = 0;
 
@@ -60,11 +72,37 @@ int read_input_windows(char* buffer, size_t size) {
 
     return result;
 }
+#elif POSIX
+int read_input_posix(char* buffer, size_t size) {
+    fd_set set;
+    FD_ZERO(&set);
+    FD_SET(STDIN_FILENO, &set);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 50000;
+
+    int result = select(STDIN_FILENO + 1, &set, NULL, NULL, &timeout);
+    if (result == -1) {
+        return 0;
+    }
+
+    if (FD_ISSET(STDIN_FILENO, &set)) {
+        ssize_t bytes_read = read(STDIN_FILENO, buffer, size);
+        // Remove the newline character from the result.
+        buffer[bytes_read - 1] = '\0';
+        return (int)bytes_read;
+    }
+
+    return 0;
+}
 #endif
 
 int read_input(char* buffer, size_t size) {
 #if WINDOWS
     return read_input_windows(buffer, size);
+#elif POSIX
+    return read_input_posix(buffer, size);
 #else
     #error "Current platform does not implement the read_input function!"
 #endif
