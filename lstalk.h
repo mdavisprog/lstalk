d18f7d328f3cbdca3e4896c9c306c97e3b6d2cf0
mdavisprog <mdavisprog@gmail.com>
2023-02-09 10:39:46 -0800

    Fix comments to satisfy warning.

--- lstalk.h
+++ lstalk.h
@@ -1783,7 +1783,7 @@ typedef struct LSTalk_SemanticTokensClientCapabilities {
      * even decide to not show any semantic tokens at all.
      *
      * requests:
-    /**
+     *
      * The client will send the `textDocument/semanticTokens/range` request
      * if the server provides a corresponding handler.
      *
@@ -2604,7 +2604,7 @@ typedef struct LSTalk_DocumentFilter {
      * - `*` to match one or more characters in a path segment
      * - `?` to match on one character in a path segment
      * - `**` to match any number of path segments, including none
-     * - `{}` to group sub patterns into an OR expression. (e.g. `**​/*.{ts,js}`
+     * - `{}` to group sub patterns into an OR expression. (e.g. `**​ / *.{ts,js}`
      *   matches all TypeScript and JavaScript files)
      * - `[]` to declare a range of characters to match in a path segment
      *   (e.g., `example.[0-9]` to match on `example.0`, `example.1`, …)
@@ -3080,7 +3080,7 @@ typedef struct LSTalk_FileOperationPattern {
      * - `*` to match one or more characters in a path segment
      * - `?` to match on one character in a path segment
      * - `**` to match any number of path segments, including none
-     * - `{}` to group sub patterns into an OR expression. (e.g. `**​/*.{ts,js}`
+     * - `{}` to group sub patterns into an OR expression. (e.g. `**​ / *.{ts,js}`
      *   matches all TypeScript and JavaScript files)
      * - `[]` to declare a range of characters to match in a path segment
      *   (e.g., `example.[0-9]` to match on `example.0`, `example.1`, …)
